67d98c00c19ff7423a03b65a0eaf835ebab2adb5
Max Alva <alva85@gmail.com>
2018-09-10 22:35:14 -0500

    first commit

---
+++ server/middlewares/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('../helpers/jwt.helper');
+const { errorResponse } = require('../helpers/response.helper');
+
+const isAuth = (req, res, next) => {
+    let token = req.get('Authorization');
+
+    if (!token) {
+        errorResponse(res, { message: 'No tienes autorización.' }, 401);
+    } else {
+        jwt.decodeToken(token)
+            .then(response => {
+                req.user = response.user;
+                next();
+            })
+            .catch((error) => {
+                errorResponse(res, { message: 'No tienes autorización.', error }, 401);
+            });
+    }
+};
+
+const isAdmin = (req, res, next) => {
+    if (req.user.role === 'ADMIN_ROLE') {
+        next();
+    } else {
+        errorResponse(res, { message: 'No tienes permisos para realizar esta acción.' }, 403);
+    }
+};
+
+module.exports = {
+    isAuth, isAdmin
+};
